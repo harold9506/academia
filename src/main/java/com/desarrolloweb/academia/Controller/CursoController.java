6d4488ea83f3c9553db216a305c759f15fcb861a
harold9506 <harold9506@hotmail.com>
2024-11-18 23:15:36 -0500

    mas
    otra

--- src/main/java/com/desarrolloweb/academia/Controller/CursoController.java
+++ src/main/java/com/desarrolloweb/academia/Controller/CursoController.java
@@ -1,19 +1,14 @@
 package com.desarrolloweb.academia.Controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.desarrolloweb.academia.Model.entity.Asignatura;
 import com.desarrolloweb.academia.Model.entity.Curso;
 import com.desarrolloweb.academia.Model.service.AcademiaServiceIface;
 
